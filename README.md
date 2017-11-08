b2890057937b34a2e4f632e0bec01f083fa948df
ngrande <devlup@outlook.com>
2017-11-08 23:30:37 +0100

    * added note about my favorite fonts * improved setup script

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ a small and very simple intro guide for myself and fellow vim beginners
 
 ## Cheatsheet
 or get inspired by this beautiful [cheatsheet](https://devhints.io/vim)
+
+
+---
+
+# Side notes
+
+## Recommended fonts
+i suggest to use a [TrueType](https://en.wikipedia.org/wiki/TrueType) font.
+
+* [Source Code Pro](https://www.archlinux.org/packages/?name=adobe-source-code-pro-fonts) (by Adobe)
+* [Droid Sans Mono](https://www.archlinux.org/packages/?name=ttf-droid)
+* [Hack](https://www.archlinux.org/packages/?name=ttf-hack) - _my favorite so far_
+
--- setup_vim_on_arch.sh
+++ setup_vim_on_arch.sh
@@ -6,6 +6,10 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 hl=$(tput smso)
 
+function print_fin() {
+	echo "${bold}installation done${normal}"
+}
+
 # install vim and plugins from arch repo
 echo "${bold}Installing vim and plugins${normal}"
 sudo pacman -S --needed \
@@ -29,19 +33,42 @@ sudo pacman -S --needed \
 	python-pygit2 \
 	python-psutil \
 
+echo "installation finished"
+
 echo "${bold} Installing vim plugins + themes from AUR (via yaourt) - press [y|Y] to continue or anything else to skip${normal}"
 read -n 1 key
 echo ""
-if [ ${key,,} = "y" ]; then
+if [ "${key,,}" = "y" ]; then
 	yaourt -S --aur --needed vim-youcompleteme-git vim-tomorrow-theme-git
+	print_fin
+fi
+
+echo "${bold} Should i install some nice TrueType monospaced fonts? - press [y|Y]${bold}"
+read -n 1 key
+echo ""
+if [ "${key,,}" = "y" ]; then
+	sudo pacman -S --needed ttf-droid adobe-source-code-pro-fonts ttf-hack
+	print_fin
 fi
 
 # setup symlinks
-echo "${bold}Setting up symlink for current user - press <ENTER> to continue${normal}"
-read
-ln -sf $(pwd)/vimrc ~/.vimrc
+echo "${bold}Setting up symlink for current user - press [y|Y] to continue${normal}"
+read -n 1 key
+echo ""
+if [ "${key,,}" = "y" ]; then
+	ln -sf $(pwd)/vimrc ~/.vimrc
+	echo "${bold}symlink for current user created${normal}"
+fi
+
+echo ""
+echo "###################"
+echo "#                 #"
+echo "#       DONE      #"
+echo "#                 #"
+echo "###################"
+echo ""
 
 #echo "${bold}Setting up symlink for root - press <ENTER> to continue${normal}"
 #read
 #sudo ln -sf $(pwd)/vimrc /etc/vimrc
-
+#echo "symlink for root created"
